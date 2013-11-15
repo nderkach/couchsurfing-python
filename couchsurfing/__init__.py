2c91161014f340d497ce0c4785e6f471aa9da2fb
Nikolay Derkach <nderk@me.com>
2013-11-15 13:08:44 +0100

    Use APi call with a date range, go back to synchronous http requests

--- couchsurfing/__init__.py
+++ couchsurfing/__init__.py
@@ -5,7 +5,8 @@ import re
 import time
 import getpass
 
-from requests_futures.sessions import FuturesSession
+# from requests_futures.sessions import FuturesSession
+import requests
 
 CS_URL = "https://api.couchsurfing.org"
 
@@ -15,23 +16,21 @@ class AuthException(Exception):
 class Api(object):
 	""" Base API class"""
 	def __init__(self, username, password):
-		# self._session = requests.Session()
-		self._session = FuturesSession(max_workers=50)
+		self._session = requests.Session()
+		# self._session = FuturesSession()
 		r = self._session.post('https://api.couchsurfing.org/sessions',
-		                      data={"username": username, "password": password}).result()
-		# print({"username": "{0}".format(username), "password": "{0}".format(password)})
+		                      data={"username": username, "password": password})
 		if (r.status_code != 200):
 			raise AuthException
 		self._uid = r.json()["url"].split('/')[-1]
+		self._username = username
 		self.get = self._session.get
-		# print(self._uid)
-		# print(r.headers)
 
 	def __repr__(self):
-		print("couchsurfing.Api()")
+		return "couchsurfing.Api()"
 
 	def __str__(self):
-		print("CouchSurfing API: Logged in as {0.realname} ({0.username})".format(self.data))
+		return "CouchSurfing API: Logged in as {0._username}".format(self)
 
 	@property
 	def uid(self):
@@ -43,7 +42,7 @@ class Messages():
 		self._api = api
 		assert(mtype in ("inbox", "sent"))
 		url = "https://api.couchsurfing.org/users/{0.uid}/messages".format(self._api)
-		r = self._api.get(url, params= {"type": mtype, "limit": limit}).result()
+		r = self._api.get(url, params= {"type": mtype, "limit": limit})
 		self.messages = r.json()['object']
 		self.version = r.json()['version']
 		self.after = r.json()['after']
@@ -54,7 +53,7 @@ class Messages():
 	def get_unread(self, with_couch_request=False):
 		for message in self.messages:
 			# TODO: send requests in parallel
-			r = self._api.get(message).result()
+			r = self._api.get(message)
 			user = r.json()["user"]["url"]
 			title = r.json()["title"]
 			date = r.json()["date"]
@@ -66,79 +65,62 @@ class Messages():
 
 class Requests(object):
 	""" Couch requests """
-	def __init__(self, api):
+	def __init__(self, api, start=0, end=None):
 		t = time.time()
 		self._api = api
-		print("Auth finished", time.time() - t)
+		# print("Initializing requests...", time.time() - t)
 		url = "https://api.couchsurfing.org/users/{0.uid}/couchrequests".format(self._api)
-		r = self._api.get(url).result()
-		
+		payload = {"since": start, "expand": "couchrequests,users"}
+
+		r = self._api.get(url, params=payload)
+
 		# print(r.json())
 
-		# print("GOT LIST OF REQUESTS", len(r.json()['object']))
 
-		self._requests = r.json()['object']
-		# print(self.requests)
-		# self._version = r.json()['version']
-		# self._after = r.json()['after']
-		# print(self._version)
-		# print(self._after)
 		self._new = []
 		self._accepted = []
+
+		if 'object' not in r.json():
+			return
+
+		self._requests = r.json()['object']
+			
 		id = 0
 
 		def get_timestamp(timestr):
 			return datetime.datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%SZ").timestamp()
 
-		request_list = set()
-
-		print("Enque all couchrequests", time.time() - t)
+		# print(len(self._requests))
 
 		for request in self._requests:
-			# print("request")
-			request_list.add(self._api.get(request))
+			# print("Request started", time.time() - t)
 
-		print("Dequeue all couchrequests", time.time() - t)
+			# print(request)
 
-		for request in request_list:
-			print("Request started", time.time() - t)
-			# print("processing")
-			r = request.result()
-			json = r.json()
-			# print(json)
+			arrival = get_timestamp(request['arrival'])
+			departure = get_timestamp(request['departure'])
 
-			arrival = get_timestamp(json['arrival'])
-			# print(arrival)
-			departure = get_timestamp(json['departure'])
+			if end and departure > end:
+				continue
 
-			# if not start < arrival < departure < end:
-			# 	# print (arrival, departure)
-			# 	continue
+			if request['status'] != "declined":
+				url = "https://couchsurfing.org/couchmanager?read={0}".format(request['url'].split('/')[-1])
+				name = "{0} from {1}".format(request["surfer_user"]["realname"], request["surfer_user"]["address"]["country"])
+				# find = re.findall(r'(.*) sent', str(request['subject']))
+				# name = find if find else request['subject']
+				# name = request["realname"]
 
-			print("Request processing started", time.time() - t)
+				if request['status'] in ("maybe", "new"):
+					self._new.append({"id": str(id), "title": name, "url": url,
+									  "class": "event-warning", "start": str(int(arrival)*1000), "end": str(int(departure)*1000) })
+					id +=1
 
-			if json['status'] == "new" and not json['is_expired']:
-				# print(json)
-				url = "https://couchsurfing.org/couchmanager?read={0}".format(json['url'].split('/')[-1])
-				find = re.findall(r'(.*) sent', str(json['subject']))
-				name = find if find else json['subject']
-				self._new.append({"id": str(id), "title": name, "url": url,
-								  "class": "event-warning", "start": str(int(arrival)*1000), "end": str(int(departure)*1000) })
-				id +=1
-				# print("append new")
+				elif request['status'] == "accepted":
+					self._accepted.append({"id": str(id), "title": name, "url": url,
+									  "class": "event-success", "start": str(int(arrival)*1000), "end": str(int(departure)*1000) })
+					id +=1
 
-			elif json['status'] == "accepted":
-				# print(json)
-				url = "https://couchsurfing.org/couchmanager?read={0}".format(json['url'].split('/')[-1])
-				find = re.findall(r'(.*) sent', str(json['subject']))
-				name = find if find else json['subject']
-				self._accepted.append({"id": str(id), "title": name, "url": url,
-								  "class": "event-info", "start": str(int(arrival)*1000), "end": str(int(departure)*1000) })
-				id +=1
-				# print("append accepted")
-			print("Request finished", time.time() - t)
-
-		print("Finished dequeuing all couchrequests", time.time() - t)
+		# print("Finished dequeuing all couchrequests", time.time() - t)
 
 		# for n in self._new:
 		# 	for a in self._accepted:
@@ -178,7 +160,9 @@ if __name__ == "__main__":
 	login = input("Login: ")
 	password = getpass.getpass()
 
-	ap = Api(login, password)
+	api = Api(login, password)
+	print(api.uid)
+	print(api)
 	messages = Messages(api, "inbox")
 
 	# now = datetime.datetime.now()
@@ -188,4 +172,4 @@ if __name__ == "__main__":
 	requests = Requests(api)
 	# print(requests.accepted)
 	# print(requests.new)
-	# print("Total: ", len(requests.accepted+requests.new))
+	print("Total: ", len(requests.accepted+requests.new))
